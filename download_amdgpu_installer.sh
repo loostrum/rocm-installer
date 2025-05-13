42f3ec7f2a75d3dbbe0e73c3c83322a048bf5e55
Leon Oostrum <l.oostrum@esciencecenter.nl>
2025-05-13 14:12:55 +0200

    Allow installing any supported rocm version

---
+++ download_amdgpu_installer.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+is_ubuntu() {
+    if ! [[ -f /etc/os-release ]]; then
+        >&2 echo "Unsupported OS"
+        return 1
+    fi
+
+    . /etc/os-release
+
+    if [[ "$ID" != "ubuntu" ]]; then
+        >&2 echo "Unsupported OS: ${ID}"
+        return 1
+    fi
+
+    return 0
+}
+
+get_ubuntu_codename() {
+    if ! [[ is_ubuntu ]]; then
+        return
+    fi
+    . /etc/os-release
+    echo ${VERSION_CODENAME}
+}
+
+get_http_status() {
+    url="$1"
+    echo $(curl -s -L -I ${url} | grep ^HTTP | tail -n 1 | cut -d$' ' -f2)
+}
+
+version="$1"
+if [[ -z "${version}" ]]; then
+    >&2 echo "No version provided"
+    exit 1
+fi
+
+ubuntu_codename=$(get_ubuntu_codename)
+
+# Url does not contain the patch release number when it's zero, e.g. 6.4.0 becomes 6.4.
+# Strip any trailing zero patch number
+regex="(.*)\.0"
+if [[ ${version} =~ ${regex} ]]; then
+    version=${BASH_REMATCH[1]}
+fi
+
+# Check if provided version is valid
+url="https://repo.radeon.com/amdgpu-install/${version}"
+if [[ $(get_http_status ${url}) -ne 200 ]]; then
+    >&2 echo "Invalid ROCm version: ${version}. Check https://repo.radeon.com/amdgpu-install for available versions"
+    exit 1
+fi
+
+# Check if Ubuntu version is supported
+url="${url}/ubuntu/${ubuntu_codename}"
+if [[ $(get_http_status ${url}) -ne 200 ]]; then
+    >&2 echo "ROCm ${version} is not available for Ubuntu ${ubuntu_codename}. Check https://repo.radeon.com/amdgpu-install/${version} for supported Ubuntu versions."
+    exit 1
+fi
+
+# Find url to installer deb file
+regex='href="(amdgpu-install.*deb)"'
+if [[ $(curl -s -L ${url}) =~ ${regex} ]]; then
+    filename=${BASH_REMATCH[1]}
+fi
+
+if [[ -z "${filename}" ]]; then
+    >&2 echo "Internal error: failed to obtain amdgpu-install url"
+    exit 1
+fi
+
+# Finally download the file
+url="${url}/${filename}"
+filename="amdgpu-install.deb"
+curl -s ${url} --output ${filename}
+echo ${filename}
